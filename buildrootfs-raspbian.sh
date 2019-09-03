8134c416b4d040b6b1fcbf76cc9e288ffc49825e
leeee-me <41778946+leeee-me@users.noreply.github.com>
2019-09-03 16:02:51 +0800

    Fixing many apt-get workflow and correct environment set for dpkg-reconfigure

--- buildrootfs-raspbian.sh
+++ buildrootfs-raspbian.sh
@@ -13,6 +13,13 @@ sudo mkdir -p rootfs/usr/share/keyrings/
 sudo cp /usr/share/keyrings/debian-archive-keyring.gpg rootfs/usr/share/keyrings/
 sudo chroot rootfs /debootstrap/debootstrap --second-stage
 
+cd rootfs
+sudo mount -t sysfs sysfs sys/
+sudo mount -t proc  proc proc/
+sudo mount -o bind /dev dev/
+sudo mount -o bind /dev/pts dev/pts
+cd ..
+
 sudo mkdir -p rootfs/etc/apt/sources.list.d/
 sudo cp ../raspbian-apt-sources.list rootfs/etc/apt/sources.list
 sudo cp ../raspbian-apt-sources-raspi.list rootfs/etc/apt/sources.list.d/raspi.list
@@ -24,31 +31,35 @@ wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key --quiet -O - | su
 sudo chroot rootfs apt-key add raspberrypi.gpg.key
 
 sudo chroot rootfs apt-get update
-sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true chroot rootfs apt-get --yes -o DPkg::Options::=--force-confdef install --no-install-recommends locales
-sudo sed -i 's/# en_US/en_US/g' rootfs/etc/locale.gen 
-sudo DEBIAN_FRONTEND=noninteractive chroot rootfs locale-gen 
-cat <<EOM > /dev/stdout | sudo tee rootfs/etc/default/locale
-LANG=en_US.UTF-8
-LANGUAGE=
-LC_CTYPE="en_US.UTF-8"
-LC_NUMERIC="en_US.UTF-8"
-LC_TIME="en_US.UTF-8"
-LC_COLLATE="en_US.UTF-8"
-LC_MONETARY="en_US.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-LC_PAPER="en_US.UTF-8"
-LC_NAME="en_US.UTF-8"
-LC_ADDRESS="en_US.UTF-8"
-LC_TELEPHONE="en_US.UTF-8"
-LC_MEASUREMENT="en_US.UTF-8"
-LC_IDENTIFICATION="en_US.UTF-8"
-LC_ALL=en_US.UTF-8
+
+sudo chroot rootfs env -i HOME="/root" PATH="/bin:/usr/bin:/sbin:/usr/sbin" TERM="$TERM" DEBIAN_FRONTEND="noninteractive" \
+	apt-get --yes -o DPkg::Options::=--force-confdef install  --no-install-recommends whiptail
+
+sudo chroot rootfs env -i HOME="/root" PATH="/bin:/usr/bin:/sbin:/usr/sbin" TERM="$TERM" \
+	apt-get --yes -o DPkg::Options::=--force-confdef install  --no-install-recommends locales tzdata keyboard-configuration
+sudo chroot rootfs env -i HOME="/root" PATH="/bin:/usr/bin:/sbin:/usr/sbin" TERM="$TERM" SHELL="/bin/bash" \
+	dpkg-reconfigure locales
+sudo chroot rootfs env -i HOME="/root" PATH="/bin:/usr/bin:/sbin:/usr/sbin" TERM="$TERM" SHELL="/bin/bash" \
+	dpkg-reconfigure tzdata
+sudo chroot rootfs env -i HOME="/root" PATH="/bin:/usr/bin:/sbin:/usr/sbin" TERM="$TERM" SHELL="/bin/bash" \
+	dpkg-reconfigure keyboard-configuration
+
+echo "raspberrypi" | sudo tee rootfs/etc/hostname
+cat <<EOM > /dev/stdout | sudo tee rootfs/etc/hosts
+127.0.0.1       localhost
+::1             localhost ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+127.0.1.1       raspberrypi
 EOM
-sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true chroot rootfs apt-get --yes -o DPkg::Options::=--force-confdef install  --no-install-recommends console-data console-common console-setup unicode-data tzdata most keyboard-configuration
 
-sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true chroot rootfs apt-get --yes -o DPkg::Options::=--force-confdef install  --no-install-recommends sudo ssh openssh-server usbmount kmod net-tools ethtool wireless-tools init iputils-ping rsyslog bash-completion ifupdown
+sudo chroot rootfs env -i hostname -F /etc/hostname
 
-sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true chroot rootfs apt-get --yes -o DPkg::Options::=--force-confdef upgrade
+sudo chroot rootfs env -i HOME="/root" PATH="/bin:/usr/bin:/sbin:/usr/sbin" TERM="$TERM" \
+	apt-get --yes -o DPkg::Options::=--force-confdef install  --no-install-recommends console-data console-common console-setup most sudo ssh openssh-server usbmount kmod net-tools ethtool wireless-tools init iputils-ping rsyslog bash-completion ifupdown 
+
+sudo chroot rootfs env -i HOME="/root" PATH="/bin:/usr/bin:/sbin:/usr/sbin" TERM="$TERM" \
+	apt-get --yes -o DPkg::Options::=--force-confdef upgrade
 
 sudo rm rootfs/raspbian.public.key
 sudo rm rootfs/raspberrypi.gpg.key
@@ -60,14 +71,12 @@ sudo chroot rootfs apt-get --yes clean
 sudo chroot rootfs apt-get --yes autoclean
 sudo chroot rootfs apt-get --yes autoremove
 
-echo "raspberrypi" | sudo tee rootfs/etc/hostname
-cat <<EOM > /dev/stdout | sudo tee rootfs/etc/hosts
-127.0.0.1       localhost
-::1             localhost ip6-localhost ip6-loopback
-ff02::1         ip6-allnodes
-ff02::2         ip6-allrouters
-127.0.1.1       raspberrypi
-EOM
+cd rootfs
+sudo umount ./dev/pts
+sudo umount ./dev
+sudo umount ./proc
+sudo umount ./sys
+cd ..
 
 cat <<"EOM" > /dev/stdout | sudo tee rootfs/etc/fstab
 proc            /proc           proc    defaults                  0       0
